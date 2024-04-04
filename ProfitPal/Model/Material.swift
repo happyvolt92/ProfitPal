fc6cce422f20212be6f9ada5b44b075398ed675c
Happyvolt92 <elodiegage@gmail.com>
2024-04-04 11:48:06 +0200

    Add model Material

---
+++ ProfitPal/Model/Material.swift
@@ -0,0 +1,17 @@
+//
+//  Material.swift
+//  ProfitPal
+//
+//  Created by HappyVolt on 04/04/2024.
+//
+
+import Foundation
+
+// Modèle représentant un matériau avec son coût
+class Material {
+    var cost: Double
+    
+    init(cost: Double) {
+        self.cost = cost
+    }
+}
---
+++ ProfitPal/ProfitCalculator.swift
@@ -0,0 +1,8 @@
+//
+//  ProfitCalculator.swift
+//  ProfitPal
+//
+//  Created by HappyVolt on 04/04/2024.
+//
+
+import Foundation
