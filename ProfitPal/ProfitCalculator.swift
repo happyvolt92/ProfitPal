cf91a9a7586e936c488e717cfb9dccd30d0a5fc1
Happyvolt92 <elodiegage@gmail.com>
2024-04-04 11:48:23 +0200

    Add calculator logic

--- ProfitPal/ProfitCalculator.swift
+++ ProfitPal/ProfitCalculator.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+
+// Protocole définissant les méthodes pour calculer le profit
+protocol ProfitCalculating {
+    func calculateProfit(materialCosts: [Material], tax: Double) -> (profit: Double, totalPrice: Double)
+}
+
+// Implémentation du calculateur de profit
+class ProfitCalculator: ProfitCalculating {
+    func calculateProfit(materialCosts: [Material], tax: Double) -> (profit: Double, totalPrice: Double) {
+        let totalCost = materialCosts.reduce(0) { $0 + $1.cost }
+        let totalTax = totalCost * (tax / 100)
+        let profit = totalCost
+        let totalPrice = totalCost + totalTax
+        return (profit, totalPrice)
+    }
+}
